55fae255cdab754297102049990d28c52010cb57
Cesar A. Herrera <Cesar@Lauren.local>
2024-04-20 16:28:50 -0700

    Refactored unit test output test for sentenceSplitter function

--- unitTests.h
+++ unitTests.h
@@ -20,27 +20,52 @@ bool fileInputTest2() {
     return result == expected;
 }
 
-void sentenceSplitterTest() {
+bool sentenceSplitterTest() {
     std::string fileName = "../test_files/document2.txt";
+    std::vector<std::string> expectedSentencesVector = {
+            "The first story is about connecting the dots",
+            "I dropped out of Reed College after the first 6 months, but then stayed around as a drop-in for another 18 months or so before I really quit",
+            "So why did I drop out",
+            "It started before I was born",
+            "My biological mother was a young, unwed college graduate student, and she decided to put me up for adoption",
+            "She felt very strongly that I should be adopted by college graduates, so everything was all set for me to be adopted at birth by a lawyer and his wife",
+            "Except that when I popped out they decided at the last minute that they really wanted a girl",
+            "So my parents, who were on a waiting list, got a call in the middle of the night asking: \"We have an unexpected baby boy; do you want him",
+            "They said: \"Of course",
+            "My biological mother later found out that my mother had never graduated from college and that my father had never graduated from high school",
+            "She refused to sign the final adoption papers",
+            "She only relented a few months later when my parents promised that I would someday go to college"
+    };
+
     std::vector<std::string> sentencesVector;
     sentenceSplitter(fileName, sentencesVector);
-    for (const auto& sentence : sentencesVector) {
-        std::cout << "- " << sentence << std::endl;
-    }
+
+    return sentencesVector == expectedSentencesVector;
 }
 
 void runTests() {
-    if (fileInputTest1()) {
-        std::cout << "Passed File Input Test 1" << std::endl;
-    } else {
+
+    std::cout << "Begin testing..." << std::endl;
+
+    bool allTestPassing = true;
+
+    if (!fileInputTest1()) {
+        allTestPassing = false;
         std::cout << "Failed File Input Test 1" << std::endl;
     }
-    if (fileInputTest2()) {
-        std::cout << "Passed File Input Test 2" << std::endl;
-    } else {
+    if (!fileInputTest2()) {
+        allTestPassing = false;
         std::cout << "Failed File Input Test 2" << std::endl;
     }
 
-    sentenceSplitterTest();
+    if (!sentenceSplitterTest()) {
+        allTestPassing = false;
+        std::cout << "Failed sentenceSplitter() Test" << std::endl;
+    }
+
+    if (allTestPassing) {
+        std::cout << "All test passing." << std::endl;
+    }
+
 }
 #endif //FILEIOS_WORDPAIRS_UNITTESTS_H
