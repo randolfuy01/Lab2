ddcd2198d1b719d0638405102cfdd3fd6b04fdad
Randolf Uy <920265148@sfsu.edu>
2024-04-20 11:53:51 -0700

    Removed comments

--- fileIOs_wordPairs.cpp
+++ fileIOs_wordPairs.cpp
@@ -1,32 +1,24 @@
 #include "fileIOs_wordPairs.h"
-#include "fileIOs_wordPairs.h"
 
 void sentenceSplitter( std::string& fname, std::vector<std::string>& sentences) {
     std::string currentSentence;
     std::string text = getText(fname);
     for (char character : text) {
         currentSentence += character;
-
-        // Check for sentence boundaries
         if (character == '.' || character == '?') {
-            // Check for special cases: period + double quotation mark or question mark + double quotation mark
             if ((character == '.' && currentSentence.size() >= 2 && currentSentence[currentSentence.size() - 2] == '"') ||
                 (character == '?' && currentSentence.size() >= 2 && currentSentence[currentSentence.size() - 2] == '"')) {
-                continue; // Ignore as it's not the end of a sentence
+                continue;
             }
             sentences.push_back(currentSentence);
             currentSentence.clear(); // Reset current sentence
         } else if (character == '\n' || (character == ':' && currentSentence.back() == '\n')) {
-            // Newline character or colon followed by newline character marks the end of a paragraph,
-            // treat it as a sentence delimiter
             if (!currentSentence.empty()) {
                 sentences.push_back(currentSentence);
             }
-            currentSentence.clear(); // Reset current sentence
+            currentSentence.clear();
         }
     }
-
-    // Add the last sentence if it's not empty
     if (!currentSentence.empty()) {
         sentences.push_back(currentSentence);
     }
--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "fileIOs_wordPairs.h"
-#include "unitTests.h"
-
-int main() {
-    runTests();
-    return 0;
-}
