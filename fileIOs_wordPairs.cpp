00a761569ed989c98f9265e82a3cd518b927d124
Cesar A. Herrera <Cesar@Lauren.local>
2024-04-20 20:39:34 -0700

    Added wordpairMapping function declaration, testing document, and unit test

--- fileIOs_wordPairs.cpp
+++ fileIOs_wordPairs.cpp
@@ -1,5 +1,54 @@
 #include "fileIOs_wordPairs.h"
 
+// Retrieves text from a file.
+std::string getText(std::string& fname) {
+  std::ifstream inFS(fname);
+  std::string fileText;
+  if (!inFS.is_open()) {
+    if (inFS.fail()) {
+      std::cerr << "Error opening file: " << fname << " (Permission denied)" << std::endl;
+    } else {
+      std::cerr << "Error opening file: " << fname << std::endl;
+    }
+    return "";
+  }
+  std::string line;
+  while (std::getline(inFS, line)) {
+    fileText += line + '\n';
+  }
+  inFS.close();
+  return fileText;
+}
+
+// Splits file text into sentences
+void sentenceSplitter(std::string& fname, std::vector<std::string>& sentences) {
+  std::string currentSentence;
+  std::string text = getText(fname);
+  for (char character : text) {
+    // Skip if leading character is a whitespace
+    if ((isspace(character)) && currentSentence.empty()) {
+      continue;
+    }
+
+    // Skip all punctuation starting or within a sentence
+    if (character == ',' || character == ';' || character == ':' || character == '"') {
+      continue;
+    }
+
+    // End of sentence detected (period, question mark, or newline), add sentence to vector
+    if (character == '.' || character == '?' || character == '\n') {
+      addSentence(sentences, currentSentence);
+      continue;
+    }
+
+    // Append current char to sentence string
+    currentSentence += character;
+
+  }
+  // Add any remaining sentence at the end of the file; if empty, nothing will be added to sentences list
+  addSentence(sentences, currentSentence);
+}
+
 // Adds non-empty string to sentences vector and resets string to empty
 void addSentence(std::vector<std::string>& sentences, std::string& currentSentence) {
     if (!currentSentence.empty()) {
@@ -8,51 +57,7 @@ void addSentence(std::vector<std::string>& sentences, std::string& currentSenten
     }
 }
 
-// Splits file text into sentences
-void sentenceSplitter(std::string& fname, std::vector<std::string>& sentences) {
-    std::string currentSentence;
-    std::string text = getText(fname);
-    for (char character : text) {
-        // Skip if leading character is a whitespace
-        if ((isspace(character)) && currentSentence.empty()) {
-            continue;
-        }
+// Function to compute the frequency of unique, unordered word pairs from a list of sentences, storing results in a provided map.
+void wordpairMapping(std::vector<std::string>& sentences,std::map<std::pair<std::string, std::string>, int> &wordpairFreq_map) {
 
-        // Skip all punctuation starting or within a sentence
-        if (character == ',' || character == ';' || character == ':' || character == '"') {
-          continue;
-        }
-
-        // End of sentence detected (period, question mark, or newline), add sentence to vector
-        if (character == '.' || character == '?' || character == '\n') {
-            addSentence(sentences, currentSentence);
-            continue;
-        }
-
-        // Append current char to sentence string
-        currentSentence += character;
-
-    }
-    // Add any remaining sentence at the end of the file; if empty, nothing will be added to sentences list
-    addSentence(sentences, currentSentence);
-}
-
-// Retrieves text from a file.
-std::string getText(std::string& fname) {
-    std::ifstream inFS(fname);
-    std::string fileText;
-    if (!inFS.is_open()) {
-        if (inFS.fail()) {
-            std::cerr << "Error opening file: " << fname << " (Permission denied)" << std::endl;
-        } else {
-            std::cerr << "Error opening file: " << fname << std::endl;
-        }
-        return "";
-    }
-    std::string line;
-    while (std::getline(inFS, line)) {
-        fileText += line + '\n';
-    }
-    inFS.close();
-    return fileText;
 }
--- fileIOs_wordPairs.h
+++ fileIOs_wordPairs.h
@@ -1,9 +1,27 @@
 #ifndef FILEIOS_WORDPAIRS_FILEIOS_WORDPAIRS_H
 #define FILEIOS_WORDPAIRS_FILEIOS_WORDPAIRS_H
+
 #include <iostream>
 #include <vector>
 #include <fstream>
 #include <string>
+#include <map>
+
+/**
+ * @brief Retrieves text from a file.
+ *
+ * @param fname The name of the file to open.
+ * @return std::string The text from the file.
+ */
+std::string getText(std::string& fname);
+
+/**
+ * @brief Splits the text from the given file into sentences and stores them in a vector.
+ *
+ * @param fname The name of the file to process.
+ * @param sentences The vector in which to store the sentences.
+ */
+void sentenceSplitter(std::string& fname, std::vector<std::string>& sentences);
 
 /**
  * @brief If the string is not empty, adds it to the list of sentences and clears it.
@@ -14,19 +32,22 @@
 void addSentence(std::vector<std::string>& sentences, std::string& currentSentence);
 
 /**
- * @brief Splits the text from the given file into sentences and stores them in a vector.
+ * @brief Generates a mapping of unique word pairs from sentences to their frequency.
  *
- * @param fname The name of the file to process.
- * @param sentences The vector in which to store the sentences.
- */
-void sentenceSplitter( std::string& fname, std::vector<std::string>& sentences);
-
-/**
- * @brief Retrieves text from a file.
+ * This function receives a vector of sentences and generates a map where the keys are word pairs and the values
+ * are their frequency. The word pairs are case insensitive and stored in lexicographical (alphabetical) order.
+ * Word pairs only contain unique words.
  *
- * @param fname The name of the file to open.
- * @return std::string The text from the file.
+ * @param sentences A vector of raw sentences to be processed.
+ * @param wordpairFreq_map A map to hold the output data: word pairs and their frequency.
+ *
+ * @note
+ * Domain of the function: Tokens are case insensitive and white space is the token delimiter.
+ * The two words in a word-pair are different (e.g., <the, the> is not valid).
+ * Order does not matter between two words in a word-pair (e.g., <college, that> is the same as <that, college>).
+ *
+ * @warning The function does not return anything, it directly updates the wordpairFreq_map variable.
  */
-std::string getText(std::string& fname);
+void wordpairMapping(std::vector<std::string>& sentences,std::map<std::pair<std::string, std::string>, int> &wordpairFreq_map);
 
 #endif
