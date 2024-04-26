67618d3d808d55303c28795110d02c310a6f23c6
Randolf Uy <920265148@sfsu.edu>
2024-04-26 12:36:52 -0700

    Steve jobs file

--- unitTests.h
+++ unitTests.h
@@ -5,6 +5,7 @@
 // Standardize Unit Testing
 // 2 unit tests for each function: sentenceSplitter(), wordPairMapping(), freqWordPairMmap()
 // 2 end to end unit tests
+// 1 end to end unit test for the file the professor provided
 
 bool sentenceSplitterUnitTest1() {
     std::string fileName = "../test_files/document1.txt";
@@ -258,6 +259,34 @@ bool steveJobsUnitTestEndToEnd() {
     return result == expected;
 }
 
+bool steveJobsSentenceSplitterUnitTest() {
+    std::string fTestName = "../unitTestFile.txt";
+    std::vector<std::string> expected{};
+    sentenceSplitter(fTestName, expected);
+    std::vector<std::string> result = {
+            "The first story is about connecting the dots",
+            "I dropped out of Reed College after the first 6 months, but then stayed around as a drop-in for another 18 months or so before I really quit",
+            "So why did I drop out",
+            "It started before I was born",
+            "My biological mother was a young, unwed college graduate student, and she decided to put me up for adoption",
+            "She felt very strongly that I should be adopted by college graduates, so everything was all set for me to be adopted at birth by a lawyer and his wife",
+            "Except that when I popped out they decided at the last minute that they really wanted a girl",
+            "So my parents, who were on a waiting list, got a call in the middle of the night asking: \"We have an unexpected baby boy; do you want him",
+            "They said: \"Of course",
+            "My biological mother later found out that my mother had never graduated from college and that my father had never graduated from high school",
+            "She refused to sign the final adoption papers",
+            "She only relented a few months later when my parents promised that I would someday go to college"
+    };
+    bool test = false;
+    if (result.size() != expected.size()) { return test; }
+    for (size_t i = 0; i < result.size(); i++) {
+        if (result.at(i) != expected.at(i)) { return test; }
+    }
+    test = true;
+    return test;
+
+
+}
 void runTests() {
     if (sentenceSplitterUnitTest1()) {
         std::cout << "Passed Sentence Splitter Test 1" << std::endl;
@@ -303,6 +332,11 @@ void runTests() {
         std::cout << "Passed Steve-Jobs-Speech End to End Test" << std::endl;
     } else {
         std::cout << "Failed Steve-Jobs-Speech End to End Test";
+    }
+    if (steveJobsSentenceSplitterUnitTest()) {
+        std::cout << "Passed Splitter Unit Test" << std::endl;
+    } else {
+        std::cout << "Failed Splitter Unit Test" << std::endl;
     }
 }
 
